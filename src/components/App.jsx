d1c1e71b889647c2e6afa160ed8b6f25bc59367f
Kyrylo <kyrylo.mm@gmail.com>
2024-01-21 00:20:33 +0100

    Upd.

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+// Імпорт компонентів і стилів
+import React from 'react';
+import { AppContainer } from './appstyles'; // Імпорт стилів
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ContactList from './phonebook-component/contact-list/ContactList';
+import ContactForm from './phonebook-component/contact-form/ContactForm';
+import Filter from './phonebook-component/filter-contact/Filter';
+
+// Головна функція застосунку
+const App = () => {
+  return (
+    <>
+      <AppContainer>
+        <ContactForm />
+        <Filter />
+        <ContactList />
+      </AppContainer>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
