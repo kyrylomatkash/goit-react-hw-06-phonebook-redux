62cf1fef4fce8637ee8656a7caea0e71f82e7eca
Kyrylo <kyrylo.mm@gmail.com>
2024-01-21 03:50:20 +0100

    Completed
    I have added Redux state management to improve the code's modularity and maintainability

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from 'components/App';
 import { persistor, store } from './components/phonebook-component/redux/store';
-// Корнева система
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
