973fb5bcf9ddf7a438cb7786378c0e55daed4a72
Kyrylo <kyrylo.mm@gmail.com>
2024-01-21 04:05:23 +0100

    Completed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from 'components/App';
+
 import { persistor, store } from './components/phonebook-component/redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
